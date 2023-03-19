96bc9110cc5c77657f02dfa9681cc8653abc29bc
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2023-03-19 14:06:06 +0100

    [UPGRADE] updated the splash screen

--- constants/colors.ts
+++ constants/colors.ts
@@ -1,5 +1,6 @@
 const tintColorLight = '#2f95dc';
 const tintColorDark = '#fff';
+const logoBackground = "#eb895f"
 
 export default {
   light: {
@@ -16,4 +17,5 @@ export default {
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorDark,
   },
+  logoBackground
 };
